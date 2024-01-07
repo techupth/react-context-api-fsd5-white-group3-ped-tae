d1e935ab87eb8717293380c3c7e0a806e8de216a
pednaruemol <naruemol.kphet@gmail.com>
2024-01-07 18:36:11 +0700

    finished:add User.DataContext.jsx/Context API

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
+import { UserDataContext } from "./UserDataContext";
 function AppHeader() {
+  const userName = useContext(UserDataContext);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {userName.username}</h2>
     </div>
   );
 }
